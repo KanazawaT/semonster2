bc052dc65421c5e688ed4a4c6af910c042cc266f
kanazawa <e1q19024@oit.ac.jp>
2023-01-17 18:42:41 +0900

    bbb

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args){
         System.out.println("HERO!");
         Hero hero = new Hero();
-        System.out.println(hero.toString());
-    
+        //System.out.println(hero.toString());
+        Player player = new Player();
+        player.drawMonsters();
+
+        player.showDeck();
     }
 
 }
